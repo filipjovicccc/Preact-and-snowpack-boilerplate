e289726b1b77c8b1553533c539b8fef011295516
johnloccck <filipjovicccc@gmail.com>
2021-10-19 21:13:21 +0200

    initial commit

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import { h, render, Component } from 'preact';
+
+class App extends Component {
+  render() {
+    return <h1>Hello from preact</h1>;
+  }
+}
+
+export default App
---
+++ index.jsx
@@ -0,0 +1,5 @@
+ import {h, render} from 'preact';
+//  import {html} from "htm/preact"
+import App from "./App"
+
+render(<App />, document.getElementById('root'));
